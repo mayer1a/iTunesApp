5b5d79ee45627d505b678cf7bf4a42b89d1600a8
Artem V. Mayer <mayer1art@gmail.com>
2022-12-25 17:38:07 +0700

    add tabbar controller for two screens

--- iOSArchitecturesDemo/Model/Services/AppStartManager/AppStartManager.swift
+++ iOSArchitecturesDemo/Model/Services/AppStartManager/AppStartManager.swift
@@ -17,22 +17,7 @@ final class AppStartManager {
     }
     
     func start() {
-        let rootVC = SearchModuleBuilder.build()
-        rootVC.navigationItem.title = "Search via iTunes"
-        
-        let navVC = self.configuredNavigationController
-        navVC.viewControllers = [rootVC]
-        
-        window?.rootViewController = navVC
+        window?.rootViewController = MainTabBarController()
         window?.makeKeyAndVisible()
     }
-    
-    private lazy var configuredNavigationController: UINavigationController = {
-        let navVC = UINavigationController()
-        navVC.navigationBar.barTintColor = UIColor.varna
-        navVC.navigationBar.isTranslucent = false
-        navVC.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        navVC.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        return navVC
-    }()
 }
