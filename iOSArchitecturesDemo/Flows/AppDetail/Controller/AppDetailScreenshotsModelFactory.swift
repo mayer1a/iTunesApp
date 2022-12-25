8ee813c2af0fe6a64491ec41912f1b3ec13e283b
Artem V. Mayer <mayer1art@gmail.com>
2022-12-26 01:45:03 +0700

    add screenshot cell view model factory

---
+++ iOSArchitecturesDemo/Flows/AppDetail/Controller/AppDetailScreenshotsModelFactory.swift
@@ -0,0 +1,39 @@
+//
+//  AppDetailScreenshotsModelFactory.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Artem Mayer on 26.12.2022.
+//  Copyright © 2022 ekireev. All rights reserved.
+//
+
+import UIKit
+
+final class AppDetailScreenshotsModelFactory {
+
+    // MARK: - Private properties
+
+    private let imageDownloader = ImageDownloader()
+    private let dispatchGroup = DispatchGroup()
+
+    // MARK: - Functions
+
+    func construct(from app: ITunesApp, completion: @escaping ([UIImage?]) -> Void) {
+        DispatchQueue.global().async { [weak self] in
+            var images: [UIImage?] = []
+
+            app.screenshotUrls.forEach {
+                self?.dispatchGroup.enter()
+
+                self?.imageDownloader.getImage(fromUrl: $0){ (image, _) in
+                    images.append(image)
+                    self?.dispatchGroup.leave()
+                }
+            }
+
+            self?.dispatchGroup.notify(queue: .main) {
+                completion(images)
+            }
+
+        }
+    }
+}
