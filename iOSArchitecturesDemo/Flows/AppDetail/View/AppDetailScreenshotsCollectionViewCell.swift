78d406aa177e29a28a3c4d0852742fe232be0322
Artem V. Mayer <mayer1art@gmail.com>
2022-12-26 01:34:45 +0700

    add screenshots collection view cell

---
+++ iOSArchitecturesDemo/Flows/AppDetail/View/AppDetailScreenshotsCollectionViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  AppDetailScreenshotsCollectionViewCell.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Artem Mayer on 25.12.2022.
+//  Copyright © 2022 ekireev. All rights reserved.
+//
+
+import UIKit
+
+final class AppDetailScreenshotsCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - Private properties
+
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.backgroundColor = .white
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        return imageView
+    }()
+
+    // MARK: - Constructions
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureComponents()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureComponents()
+    }
+
+    // MARK: - Lifecycle
+
+    override func prepareForReuse() {
+        imageView.image = nil
+    }
+
+    // MARK: - Functions
+
+    func setupData(_ image: UIImage?) {
+        imageView.image = image
+    }
+    
+    // MARK: - Private functions
+
+    private func configureComponents() {
+        contentView.addSubview(imageView)
+
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
