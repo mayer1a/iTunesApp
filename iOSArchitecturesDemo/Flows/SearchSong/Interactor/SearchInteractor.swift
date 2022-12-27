4dbbd04b857844cbb409b4520d0630cb3a6fc1a4
Artem V. Mayer <mayer1art@gmail.com>
2022-12-28 00:51:42 +0700

    add fetch image method

--- iOSArchitecturesDemo/Flows/SearchSong/Interactor/SearchInteractor.swift
+++ iOSArchitecturesDemo/Flows/SearchSong/Interactor/SearchInteractor.swift
@@ -11,6 +11,7 @@ import Alamofire
 
 protocol SearchInteractorInput {
     func requestSongs(with query: String, completion: @escaping (Result<[ITunesSong]>) -> Void)
+    func getImage(fromUrl url: URLConvertible, completion: @escaping DownloadImageCompletion)
 }
 
 final class SearchInteractor: SearchInteractorInput {
@@ -18,17 +19,25 @@ final class SearchInteractor: SearchInteractorInput {
     // MARK: - Private properties
 
     private let searchService: ITunesSearchService
+    private let downloaderService: ImageDownloader
 
     // MARK: - Construction
 
-    init(searchService: ITunesSearchService) {
+    init(searchService: ITunesSearchService, downloaderService: ImageDownloader) {
         self.searchService = searchService
+        self.downloaderService = downloaderService
     }
 
     // MARK: - Functions
 
     func requestSongs(with query: String, completion: @escaping (Alamofire.Result<[ITunesSong]>) -> Void) {
         searchService.getSongs(forQuery: query, completion: completion)
+    }
+
+    func getImage(fromUrl url: URLConvertible, completion: @escaping DownloadImageCompletion) {
+        DispatchQueue.global().async { [weak self] in
+            self?.downloaderService.getImage(fromUrl: url, completion: completion)
+        }
     }
 
 }
